fb6881fef4cff8c5ada5b35fa48480c82c1523b3
Praman Ghimire <pramang017357@nec.edu.np>
2024-06-13 12:44:23 +0545

    Api created

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $student = Student::all();
+        $data = [
+            'status' => 200,
+            'student' => $student
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $student = $request->only([
+                'name',
+                'email',
+                'phone'
+            ]);
+
+            $student_data = Student::create($student);
+
+            $data = [
+                'status' => 200,
+                'student' => $student_data
+            ];
+
+            return response()->json($data, 200);
+        } 
+        catch (\Exception $e) {
+
+            return response()->json([
+                'status' => 500,
+                'error' => 'Server error: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('user');
+Route::get('student',[ApiController::class,'index']);
+
+Route::post('student',[ApiController::class,'store']);
